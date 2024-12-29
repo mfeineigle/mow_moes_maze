f6461720074bcca882d978842da3b55f0ffdb6b5
Mark Feineigle <mfeineigle@gmail.com>
2024-12-29 16:38:20 -0500

    check for area instead of body (player now area)

--- tile.gd
+++ tile.gd
@@ -3,12 +3,14 @@ extends Area2D
 @export var height: int = 1
 var is_cut: bool = false
 
+
 func _ready() -> void:
 	$Uncut.visible = true
 	$Cut.visible = false
 	
-func _on_player_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+
+func _on_area_shape_player_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
+	if area.is_in_group("player"):
 		if is_cut:
 			return
 		else:
