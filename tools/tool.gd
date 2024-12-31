31c6a80756c2a117c94464b5ba0b6c0729704cf8
Mark Feineigle <mfeineigle@gmail.com>
2024-12-31 03:55:16 -0500

    add fuel and stamina costs to tools

--- tools/tool.gd
+++ tools/tool.gd
@@ -4,6 +4,11 @@ class_name Tool
 ## Cost per tile traversed
 @export var stamina_cost: int
 
+@export var fuel_cost: int
+
 # each tool should have a move function
 # orients the tool in the correct dir
 #func move(dir) -> void:
+
+func activate() -> int:
+	return 0
