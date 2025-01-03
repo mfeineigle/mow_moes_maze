5a4cc09fb131913af9727e2a81fe8d1a1b18c580
Mark Feineigle <mfeineigle@gmail.com>
2025-01-02 20:32:58 -0500

    add strafing

--- characters/player.gd
+++ characters/player.gd
@@ -10,7 +10,6 @@ extends Area2D
 @onready var current_tool: Tool
 
 var cur_dir: Vector2 = Vector2.LEFT
-var last_dir: Vector2 = Vector2.LEFT
 var at_truck: bool = false
 var stamina_level: int = 0
 var fuel_level: int = 0
@@ -20,7 +19,12 @@ var tile_size = 64
 var inputs = {"move_right": Vector2.RIGHT,
 			  "move_left": Vector2.LEFT,
 			  "move_up": Vector2.UP,
-			  "move_down": Vector2.DOWN}
+			  "move_down": Vector2.DOWN,
+			  "strafe_right": Vector2.RIGHT,
+			  "strafe_left": Vector2.LEFT,
+			  "strafe_up": Vector2.UP,
+			  "strafe_down": Vector2.DOWN,
+			}
 
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
@@ -40,14 +44,14 @@ func _unhandled_input(event):
 		GameEvents.fuel_changed.emit(fuel_level)
 	if at_truck and event.is_action_pressed("change_tool"):
 		GameEvents.changed_tool.emit(self)
-		print("changing")
+
 
 func _process(_delta: float) -> void:
 	if moving:
 		pass
 	else:
 		for dir in inputs.keys():
-			if Input.is_action_pressed(dir):
+			if Input.is_action_pressed(dir, true):
 				move(dir)
 				for tool in tools.get_children():
 					tool.move(dir)
@@ -57,12 +61,24 @@ func move(dir):
 	match dir:
 		"move_left":
 			animation_player.play("move_left")
+			cur_dir = Vector2.LEFT
 		"move_right":
 			animation_player.play("move_right")
+			cur_dir = Vector2.RIGHT
 		"move_up":
 			animation_player.play("move_up")
+			cur_dir = Vector2.UP
 		"move_down":
 			animation_player.play("move_down")
+			cur_dir = Vector2.DOWN
+		"strafe_left":
+			_strafe()
+		"strafe_right":
+			_strafe()
+		"strafe_up":
+			_strafe()
+		"strafe_down":
+			_strafe()
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
 	if not ray.is_colliding():
@@ -82,3 +98,13 @@ func move(dir):
 		GameEvents.steps_changed.emit()
 	else:
 		print("hit something")
+
+func _strafe() -> void:
+	if cur_dir == Vector2.UP:
+		animation_player.play("move_up")
+	elif cur_dir == Vector2.DOWN:
+		animation_player.play("move_down")
+	elif cur_dir == Vector2.LEFT:
+		animation_player.play("move_left")
+	elif cur_dir == Vector2.RIGHT:
+		animation_player.play("move_right")
